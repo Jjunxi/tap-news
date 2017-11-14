3f3d612fa0bc2b2e4cd16d177147d77110f82002
jjunxi417 <junxi@jjunxi417s-MacBook-Pro.local>
2017-11-14 23:34:47 +0800

    NewsPanel NewsCard

---
+++ web/web-client/src/App/App.js
@@ -0,0 +1,24 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+import React, { Component } from 'react';
+import './App.css';
+import logo from './logo.png'
+
+import NewsPanel from '../NewsPanel/NewPanel';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <img className='logo' src={logo} alt='logo' />
+        <div className='container'>
+          <NewsPanel />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
